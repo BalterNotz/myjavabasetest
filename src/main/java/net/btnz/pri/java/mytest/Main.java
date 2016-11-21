0f8a646c44b9973b494faf44f1ab087b4a3c265e
zhangsongwei <zhangsongwei@jk.cn>
2016-11-21 11:55:19 +0800

    main

---
+++ src/main/java/net/btnz/pri/java/mytest/Main.java
@@ -0,0 +1,10 @@
+package net.btnz.pri.java.mytest;
+
+/**
+ * Created by zhangsongwei on 2016/11/21.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
