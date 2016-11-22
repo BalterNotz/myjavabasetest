4f8d4a8ca6160b5b86fd1f2920a725672c95ed8d
zhangsongwei <zhangsongwei@jk.cn>
2016-11-22 10:25:11 +0800

    Collection使用

--- src/main/java/net/btnz/pri/java/mytest/Main.java
+++ src/main/java/net/btnz/pri/java/mytest/Main.java
@@ -1,10 +1,13 @@
 package net.btnz.pri.java.mytest;
 
+import java.util.Vector;
+
 /**
  * Created by zhangsongwei on 2016/11/21.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Vector<Integer> vector = new Vector<Integer>();
     }
 }
---
+++ src/main/java/net/btnz/pri/java/mytest/exceptionm/ExceptionM.java
@@ -0,0 +1,29 @@
+package net.btnz.pri.java.mytest.exceptionm;
+
+/**
+ * Created by zhangsongwei on 2016/11/21.
+ */
+public class ExceptionM {
+    public static void main(String[] args) throws Exception {
+        System.out.println(test());
+    }
+
+    public static String test() throws Exception {
+        try {
+            print("in try");
+            throw new Exception("Exception");
+//            return "returnreturn in try";
+        } catch (Exception e) {
+            throw e;
+//            return print(e.getMessage());
+        } finally {
+//            return print("in finally");
+        }
+    }
+
+    public static String print(String str) {
+        System.out.println(str);
+        return str;
+    }
+
+}
