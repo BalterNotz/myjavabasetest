c50a106b36ab3db03ee4b59470164e8326cf8f67
zhangsongwei <zhangsongwei@jk.cn>
2016-12-09 20:28:46 +0800

    java-explore

---
+++ src/main/java/net/btnz/pri/java/myjavabasetest/loader/DemoMain.java
@@ -0,0 +1,10 @@
+package net.btnz.pri.java.myjavabasetest.loader;
+
+/**
+ * Created by zhangsongwei on 2016/12/9.
+ */
+public class DemoMain {
+    public static void main(String[] args){
+        System.out.println("Hello world! this is inside the DemoMain class.");
+    }
+}
