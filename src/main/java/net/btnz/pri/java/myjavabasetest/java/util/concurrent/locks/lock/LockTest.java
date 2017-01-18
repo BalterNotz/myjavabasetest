4c491b5c7f3ec085eece44db36739493a8524996
zhangsongwei <zhangsongwei@jk.cn>
2017-01-18 19:09:10 +0800

    concurrent lock init

---
+++ src/main/java/net/btnz/pri/java/myjavabasetest/java/util/concurrent/locks/lock/LockTest.java
@@ -0,0 +1,10 @@
+package net.btnz.pri.java.myjavabasetest.java.util.concurrent.locks.lock;/**
+ * Created by zhangsongwei on 2017/1/18.
+ */
+
+/**
+ * @author: Balter Notz [BalterNotz@foxmail.com]
+ * @Date: 2017/1/18 10:23
+ */
+public class LockTest {
+}
