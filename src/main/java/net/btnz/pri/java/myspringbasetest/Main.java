285800f8b66aa7fc86ab496ef311cbbde63eec7c
BalterNotz <BalterNotz@foxmail.com>
2018-03-13 17:26:10 +0800

    引入springboot开始

--- src/main/java/net/btnz/pri/java/myspringbasetest/Main.java
+++ src/main/java/net/btnz/pri/java/myspringbasetest/Main.java
@@ -1,4 +1,8 @@
 package net.btnz.pri.java.myspringbasetest;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 }
